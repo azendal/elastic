30de5e3f78f2065ea19ba3c62365eb990007c4e2
fernando trasvina <trasvina@gmail.com>
2008-11-04 11:33:49 -0600

    add: support for elastic three-column based layout

--- helpers.js
+++ helpers.js
@@ -1,10 +1,26 @@
+/**
+ * Elastic Helpers
+ *
+ * Helper functions serve to easily accomplish common task that either are too dificult to implement with pure css
+ * or are only possible with javascript.
+ * That is why helpers serve as a compliment for the DOM so classes have an expected behaviour easily
+ *
+ * LICENSE: It is planned to release it as MIT
+ *
+ * @author     Fernando Trasviña
+ * @copyright  2008 Elastic css framework
+ * @license    MIT
+ * @version    0.1
+ * @link       elastic/dev/helpers.js
+ * @since      0.1
+*/
 (function($){
 	$(function(){
-		$('div.two-columns > div.fixed-left-column').each(function(){
+		$('div.two-columns > div.fixed-left-column, div.three-columns > div.fixed-left-column').each(function(){
 			$('div.elastic-column', this.parentNode).css('margin-left', $(this).css('width'))
 		});
 		
-		$('div.two-columns > div.fixed-right-column').each(function(){
+		$('div.two-columns > div.fixed-right-column, div.three-columns > div.fixed-right-column').each(function(){
 			$('div.elastic-column', this.parentNode).css('margin-right', $(this).css('width'))
 		});
 		
@@ -12,9 +28,8 @@
 			var maxHeight = 0;
 			$('> div', this).each(function(){
 				maxHeight = (this.clientHeight > maxHeight) ? this.clientHeight : maxHeight;
-			});
-			$('> div', this).each(function(){
-				$(this).css('height', maxHeight)
+			}).each(function(){
+				$(this).css('height', maxHeight);
 			});
 		});
 		
